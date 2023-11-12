4a1124bfe700648b8a1c38385e3fa04441afdef3
xiantang <zhujingdi1998@gmail.com>
2023-11-13 01:08:54 +0800

    support upload course from https://english-e-reader.net/top/read

--- upload.py
+++ upload.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-from os.path import isfile, join
-from os import listdir
-import sys
-import requests
+from __future__ import print_function
+
+import argparse
 import os
+from glob import glob
+from os.path import basename
+
+import ebooklib
+import requests
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from ebooklib import epub
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 load_dotenv()
@@ -13,30 +19,131 @@ key = os.getenv("APIKey")
 postAddress = os.getenv("postAddress")
 mypath = os.getenv("mypath")
 status = os.getenv("status")
-collectionID = os.getenv("collectionID")
 
 
-def __main__():
-    # Create list/array of mp3
-    mp3s = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    mp3s.sort(reverse=False)
-    for mp3 in mp3s:
-       
-        m = MultipartEncoder([
-                    ('title', mp3[0:11]),
-                    ('text', mp3[0:11]),
-                    ('status',status),
-                    ('collection',collectionID),
-                    ('audio', (mp3, open(mypath + '/' + mp3, 'rb'), 'audio/mpeg'))]   
-                )
-
-        h = {'Authorization': key,
-            'Content-Type': m.content_type}
-
-        r = requests.post(postAddress, data=m, headers=h)
-        print(r.status_code)
-        print(r.text)
+parser = argparse.ArgumentParser(description="a tool for Upload audio book to lingq.")
+parser.add_argument("-a", "--audio_folder")
+parser.add_argument("-b", "--book_path")
+parser.add_argument("-t", "--title")
+args = parser.parse_args()
 
 
-if __name__ == "__main__":
-    __main__()
+if not (args.audio_folder or args.book_path or args.title):
+    parser.error("No action requested, add --audio_folder or --book_path or --title")
+
+
+header = {"Authorization": key, "Content-Type": "application/json"}
+
+title = args.title
+discriprtion = """
+"""
+
+book = epub.read_epub(args.book_path)
+
+cover = glob(args.audio_folder + "/*.jpg")
+
+
+def chapter_to_str(doc):
+    soup = BeautifulSoup(doc.content, "html.parser")
+    text = [para.get_text() for para in soup.find_all("p")]
+    return " ".join(text)
+
+
+def create_collections(
+    title,
+    description,
+):
+    url = "https://www.lingq.com/api/v3/en/collections/"
+    body = {
+        "description": description,
+        "hasPrice": False,
+        "isFeatured": False,
+        "sourceURLEnabled": False,
+        "language": "en",
+        "level": 1,
+        "sellAll": False,
+        "tags": [],
+        "title": title,
+    }
+    r = requests.post(
+        url,
+        json=body,
+        headers=header,
+    )
+    return r.json()["id"]
+
+
+def upload_cover(cover_path, collectonID):
+    print("uploading cover ...")
+    m = MultipartEncoder(
+        [
+            ("image", (cover_path, open(cover_path, "rb"), "image/jpg")),
+        ]
+    )
+    h = {"Authorization": key, "Content-Type": m.content_type}
+    url = "https://www.lingq.com/api/v3/en/collections/" + str(collectonID)
+    r = requests.patch(
+        url=url,
+        data=m,
+        headers=h,
+    )
+    print(r.json())
+
+
+def get_lessons(collectonID):
+    url = (
+        "https://www.lingq.com/api/v3/en/collections/"
+        + str(collectonID)
+        + "/lessons/?page=1&page_size=100&sortBy=pos"
+    )
+    r = requests.get(
+        url,
+        headers=header,
+    )
+    return r.json()
+
+
+def upload_lessons(collectionID):
+    listofmp3s = glob(args.audio_folder + "/*.mp3")
+    listofmp3s.sort()
+    list_book_charpter = []
+    for c in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+        if "split" in c.get_name():
+            list_book_charpter.append(c)
+
+    for doc, audiofile in list(zip(list_book_charpter, listofmp3s)):
+        s = chapter_to_str(doc)
+        mp3name = basename(audiofile)
+        title = mp3name.split(".")[0]
+        print("creating lesson " + title + " ...")
+        body = {
+            "title": title,
+            "status": status,
+            "collection": collectionID,
+            "text": s,
+        }
+        h = {"Authorization": key, "Content-Type": "application/json"}
+        r = requests.post(postAddress, json=body, headers=h)
+        lesson_id = r.json()["id"]
+        print("uploading audiofile...")
+        body = [
+            ("language", "en"),
+            ("audio", (audiofile, open(audiofile, "rb"), "audio/mpeg")),
+        ]
+        if len(cover) > 0:
+            body.append(("image", (cover[0], open(cover[0], "rb"), "image/jpg")))
+
+        m = MultipartEncoder(body)
+        h = {"Authorization": key, "Content-Type": m.content_type}
+        r = requests.patch(
+            "https://www.lingq.com/api/v3/en/lessons/" + str(lesson_id) + "/",
+            data=m,
+            headers=h,
+        )
+
+
+collectionID = create_collections(title, discriprtion)
+if len(cover) > 0:
+    upload_cover(cover[0], collectionID)
+
+upload_lessons(collectionID)
