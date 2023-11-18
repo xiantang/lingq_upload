d7ca911883f40d8383e92e05e52aad0d83c9e699
xiantang <zhujingdi1998@gmail.com>
2023-11-18 23:20:42 +0800

    commit

--- fetch_meta_data.py
+++ fetch_meta_data.py
@@ -17,7 +17,7 @@ def extract_english_level_revised(html_content):
 
     # Searching for the English level in the vicinity of the provided snippet
     # The level seems to be within a <a> tag inside a <p> tag with class 'text-center bg-danger'
-    level_tag = soup.find("p", class_="text-center bg-danger")
+    level_tag = soup.find("dlv", class_="col-md-3 col-sm-12 hidden-sm hidden-xs")
     if level_tag and level_tag.find("a"):
         return level_tag.find("a").get_text(strip=True)
 
@@ -37,21 +37,28 @@ def extract_tags_corrected(html_content):
     return tags
 
 
-def map_english_levels(original_level):
-    # Dictionary mapping the original levels to the new levels
-    level_mapping = {
-        "A1 Starter": "Beginner 1",
-        "A2 Elementary": "Beginner 2",
-        "B1 Pre-Intermediate": "Intermediate 1",
-        "B1+ Intermediate": "Intermediate 2",
-        "B2 Intermediate-Plus": "Advanced 1",
-        "B2+ Upper-Intermediate": "Advanced 2",
-        "C1 Advanced": "Advanced 1",
-        "C2 Unabridged": "Advanced 2",
+def find_english_level_in_html(html_content):
+    # Dictionary of English levels to search for
+    levels_to_find = {
+        "A1 Starter": "A1 Starter",
+        "A2 Elementary": "A2 Elementary",
+        "B1 Pre-Intermediate": "B1 Pre-Intermediate",
+        "B1+ Intermediate": "B1+ Intermediate",
+        "B2 Intermediate-Plus": "B2 Intermediate-Plus",
+        "B2+ Upper-Intermediate": "B2+ Upper-Intermediate",
+        "C1 Advanced": "C1 Advanced",
+        "C2 Unabridged": "C2 Unabridged",
     }
 
-    # Return the corresponding new level
-    return level_mapping.get(original_level, "Unknown Level")
+    # Parsing the HTML content
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    # Searching for any of the specified English levels
+    for level in levels_to_find:
+        if soup.find(string=lambda text: text and level in text):
+            return levels_to_find[level]
+
+    return "English level not found in document"
 
 
 # Function to extract information from an HTML file
@@ -78,9 +85,8 @@ def extract_info_from_html(html_content):
         if meta_description
         else "Book description not found"
     )
-    level = extract_english_level_revised(html_content)
+    level = find_english_level_in_html(html_content)
     tags = extract_tags_corrected(html_content)
-    print(level)
 
     return {
         "title": title,
@@ -89,6 +95,23 @@ def extract_info_from_html(html_content):
         "description": description,
         "tags": tags,
     }
+
+
+def map_english_levels(original_level):
+    # Dictionary mapping the original levels to the new levels
+    level_mapping = {
+        "A1 Starter": "Beginner 1",
+        "A2 Elementary": "Beginner 2",
+        "B1 Pre-Intermediate": "Intermediate 1",
+        "B1+ Intermediate": "Intermediate 2",
+        "B2 Intermediate-Plus": "Advanced 1",
+        "B2+ Upper-Intermediate": "Advanced 2",
+        "C1 Advanced": "Advanced 1",
+        "C2 Unabridged": "Advanced 2",
+    }
+
+    # Return the corresponding new level
+    return level_mapping.get(original_level, "Unknown Level")
 
 
 content = requests.get("https://english-e-reader.net/book/" + args.title).content
